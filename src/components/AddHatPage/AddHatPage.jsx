075e314c029ec010301401a5ac306a66308282a3
Adam Iverson <aciverso@gmail.com>
2021-12-21 13:59:55 -0600

    form wireframe

--- src/components/AddHatPage/AddHatPage.jsx
+++ src/components/AddHatPage/AddHatPage.jsx
@@ -1,7 +1,44 @@
+import react, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+
 function AddHatPage() {
+
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+
+  
   return (
     <>
       <h1>Add Hat</h1>
+      <form>
+        <input
+        type="text"
+        placeholder='description'
+        />
+        <input
+        type="text"
+        placeholder='photo url'
+        />
+        <input
+        type="text"
+        placeholder='hat color'
+        />
+        <input
+        type="text"
+        placeholder='hat style'
+        />
+        <input
+        type="text"
+        placeholder='hat fabric'
+        />
+        <input
+        type="text"
+        placeholder='hat vibe'
+        />
+      </form>
+
     </>
   )
 }
