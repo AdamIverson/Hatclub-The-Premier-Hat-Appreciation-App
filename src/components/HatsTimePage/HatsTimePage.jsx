80d9288e65aa1d7b745625cc6995c555cbe7113e
Adam Iverson <aciverso@gmail.com>
2021-12-28 10:38:17 -0600

    committing just prior to making the navbar a Container - sizing is not functional for hats nor navbar

--- src/components/HatsTimePage/HatsTimePage.jsx
+++ src/components/HatsTimePage/HatsTimePage.jsx
@@ -1,5 +1,7 @@
 import react, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Box, Button, Card, ImageList, ImageListItem, ImageListItemBar, TextField, NativeSelect } from "@mui/material";
+
 
 export default function HatsTimePage() {
   const dispatch = useDispatch();
@@ -11,36 +13,76 @@ export default function HatsTimePage() {
   }, []);
 
   const favHat = (id) => {
-    console.log('fav button click');
-  }
+    console.log("fav button click");
+  };
 
   const deleteHat = (id) => {
-    console.log('delete button click');
-    dispatch ({
-      type: 'DELETE_HAT',
-      payload: id
-    })
-  }
+    console.log("delete button click");
+    dispatch({
+      type: "DELETE_HAT",
+      payload: id,
+    });
+  };
 
   return (
-    <>
-      <h1>HATS TIME</h1>
-      <ul>
-        {hats.map((hat) => {
-          return (
-            <li key={hat.id}>
-              <img height="300px" src={hat.photo_url}/>
-              <p>{hat.description}</p>
-              <p>{hat.hat_color}</p>
-              <p>{hat.hat_style}</p>
-              <p>{hat.hat_fabric}</p>
-              <p>{hat.hat_vibe}</p>
-              <button onClick={favHat}>FAV</button>
-              <button onClick={e => deleteHat(hat.id)}>DELETE</button>
-            </li>
-          )
-        })}
-      </ul>
-    </>
+    <Box
+      sx={{
+        width: 1000,
+        height: 900
+
+      }}
+    >
+      <ImageList 
+        sx={{ width: 1000, height: 900 }} 
+        cols={3} 
+        rowHeight={164}
+        // variant="masonry"
+        >
+        {hats.map((hat) => (
+          <ImageListItem key={hat.id}>
+            <img
+              src={`${hat.photo_url}?w=450&h=450&fit=crop&auto=format`}
+              srcSet={`${hat.photo_url}?w=450&h=450&fit=crop&auto=format&dpr=2 2x`}
+              alt={hat.description}
+              loading="lazy"
+            />
+            <ImageListItemBar position="below" title={hat.description} />
+            <Button 
+              onClick={favHat}
+              variant="contained"
+              >
+                FAV
+            </Button>
+            <Button 
+              onClick={e => deleteHat(hat.id)}
+              variant="outlined"
+              color="warning"
+              >
+                DELETE
+            </Button>
+          </ImageListItem>
+        ))}
+      </ImageList>
+    </Box>
+
+    // <>
+    //   <h1>HATS TIME</h1>
+    //   <ul>
+    //     {hats.map((hat) => {
+    //       return (
+    //         <li key={hat.id}>
+    //           <img height="300px" src={hat.photo_url}/>
+    //           <p>{hat.description}</p>
+    //           <p>{hat.hat_color}</p>
+    //           <p>{hat.hat_style}</p>
+    //           <p>{hat.hat_fabric}</p>
+    //           <p>{hat.hat_vibe}</p>
+    //           <button onClick={favHat}>FAV</button>
+    //           <button onClick={e => deleteHat(hat.id)}>DELETE</button>
+    //         </li>
+    //       )
+    //     })}
+    //   </ul>
+    // </>
   );
 }
